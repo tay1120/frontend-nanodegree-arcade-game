1e6cfb87141efdcfee0619c8aa9c7370d6646597
Tay Clarke <tavia1120@yahoo.com>
2015-03-08 16:31:27 -0400

    Added variable player

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
