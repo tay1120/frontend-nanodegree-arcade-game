4fd646cea7f7b040e802954f2fd446507c6da586
Tay Clarke <tavia1120@yahoo.com>
2015-03-08 16:26:25 -0400

    Added Player class function and Player Image

--- js/app.js
+++ js/app.js
@@ -24,7 +24,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+//Add player class
+var Player = function() {
+    // Add image for player
+    this.sprite = 'images/char-horn-girl.png';
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
